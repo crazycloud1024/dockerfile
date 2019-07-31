01d82809e2f53ee98d2403970e1cdf2fbdf64724
luoqian67 <originalsvampire67@gmail.com>
2019-07-31 17:40:48 +0800

    修改python3-dockerfile

---
+++ alpine-py3-base/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+source /root/.pyenv/completions/pyenv.bash
+
+pip install tox
+
+exec $@
--- benchtools/script/test_nginx.py
+++ benchtools/script/test_nginx.py
@@ -4,30 +4,30 @@ from locust import HttpLocust, TaskSet, task
 
 class Task(TaskSet):
 
-#     def on_start(self):
-#         self.index()
- #        self.login()
+    #     def on_start(self):
+    #         self.index()
+    #         self.login()
 
- #    def on_stop(self):
- #        self.logot()
+    #     def on_stop(self):
+    #         self.logot()
 
- #    def login(self):
- #        self.client.get("/info")
+    #     def login(self):
+    #         self.client.get("/info")
 
- #    def logot(self):
- #        self.client.post("/logot", {"username":"test", "password": "test"})
+    #     def logot(self):
+    #         self.client.post("/logot", {"username": "test", "password": "test"})
 
-    @task(2)
+    @task
     def index(self):
         self.client.get("/")
 
-    @task(1)
-    def info(self):
-        self.client.get("/info")
+#     @task(1)
+#     def info(self):
+#         self.client.get("/info")
+
 
 class WebsiteUser(HttpLocust):
     task_set = Task
-    wait_function = lambda self: random.expovariate(1)*1000 
-    # min_wait = 2000
-    # max_wait = 4000
-
+    def wait_function(self): return random.expovariate(1)*1000
+#     min_wait = 2000
+#     max_wait = 4000
